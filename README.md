0c8af391fb8459110b7bfc5658c65a0b347301d7
Milos <milosbulatovic3@gmail.com>
2017-01-17 23:32:57 +0100

    use jinja2 for templates. added a few page functions, only basic setup, added template files.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Score Alarm backend application using CherryPy Python web framework.
 
 Requirements:
+
 sudo apt-get install postgresql psycopg2
-pip install cherrypy psycopg2
+
+pip install cherrypy psycopg2 jinja2
--- site.py
+++ site.py
@@ -1,12 +1,34 @@
 import os.path
 import cherrypy
+from jinja2 import Environment, FileSystemLoader
 
+env = Environment(loader=FileSystemLoader('templates'))
 
 class ScoreAlarm:
 
 	@cherrypy.expose
 	def index(self):
-		return 'Score Alarm application server'
+		template = env.get_template('index.html')
+		content = 'Score Alarm application server'
+		return template.render({'content': content})
+
+	@cherrypy.expose
+	def add_game(self):
+		template = env.get_template('add_game.html')
+		content = 'Input data here:'
+		return template.render({'content': content})
+
+	@cherrypy.expose
+	def edit_game(self, pk):
+		template = env.get_template('edit_game.html')
+		content = 'Input data here:'
+		return template.render({'content': content})
+
+	@cherrypy.expose
+	def delete_game(self, pk):
+		template = env.get_template('delete_game.html')
+		content = 'Input data here:'
+		return template.render({'content': content})
 
 sa_config = os.path.join(os.path.dirname(__file__), 'score_alarm.conf')
 
